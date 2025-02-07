45eaf3bb4ffc858ac33c91a0943198074b501b2a
Nowshin Chowdhury <80466283+Nowshin17@users.noreply.github.com>
2025-02-08 02:59:35 +0600

    Added pagination on the list of git repo

--- lib/service_page/service.dart
+++ lib/service_page/service.dart
@@ -6,17 +6,28 @@ import '../models/repo_model.dart';
 class DataFromAPI {
   static const String baseUrl = baseURL;
 
-  Future<List<GitRepo>> fetchGitReposFromAPI() async {
-    final response = await http.get(Uri.parse(baseUrl));
+  Future<Map<String, dynamic>> fetchGitReposFromAPI({int page = 1, int perPage = 30}) async {
+    final response = await http.get(Uri.parse("$baseUrl&page=$page"));
 
     if (response.statusCode == 200) {
       print("Response: 200");
       final data = json.decode(response.body);
-      print(data);
       final List<dynamic> items = data['items'];
-      return items.map((repo) => GitRepo.fromJson(repo)).toList();
+      final repos = items.map((repo) => GitRepo.fromJson(repo)).toList();
+
+      String? linkHeader = response.headers['link'];
+      int totalPages = _parseLastPage(linkHeader);
+
+      return {'repos': repos, 'totalPages': totalPages};
     } else {
       throw Exception("Failed to fetch repositories");
     }
+  }
+
+  int _parseLastPage(String? linkHeader) {
+    if (linkHeader == null) return 1;
+    RegExp regExp = RegExp(r'&page=(\d+)>; rel="last"');
+    Match? match = regExp.firstMatch(linkHeader);
+    return match != null ? int.parse(match.group(1)!) : 1;
   }
 }
